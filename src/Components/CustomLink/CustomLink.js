74e081227fef2b46cdfe95fd31bcb6dd12be4c5e
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-04-04 12:41:14 +0600

    components created and fonts added

--- src/Components/CustomLink/CustomLink.js
+++ src/Components/CustomLink/CustomLink.js
@@ -6,7 +6,7 @@ const CustomLink = ({ children, to, ...props }) => {
   let match = useMatch({ path: resolved.pathname, end: true });
   return (
     <div>
-      <Link style={{ color: match ? "#0991B1" : "BLACK" }} to={to} {...props}>
+      <Link style={{ color: match ? "#4CAF50" : "BLACK" }} to={to} {...props}>
         {children}
       </Link>
     </div>
